2ad27aa44a6590784791c8d8cdf58770b030bb5c
Keenen Roshan <keenenroshan@192-168-1-106.tpgi.com.au>
2023-08-14 00:02:34 +1000

    Added a routing system using react-router-dom and linked all current tabs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './Components/Navbar';
-import { createTheme, ThemeProvider } from '@mui/material';
 
+import Home from './Components/Home';
+import Navbar from './Components/Navbar';
+import TicTacToe from './Components/TicTacToe';
+import ColourMania from './Components/ColourMania';
+import Hangman from './Components/Hangman';
+import About from './Components/About';
+import Education from './Components/Education';
+import Work from './Components/Work';
+import Contact from './Components/Contact';
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from 'react-router-dom';
+
+import { createTheme, ThemeProvider } from '@mui/material';
 const theme = createTheme({
   typography: {
     fontFamily: [
@@ -15,7 +31,19 @@ function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
+        <BrowserRouter>
           <Navbar />
+          <Routes>
+            <Route path='' element={<Home />}/>
+            <Route path='/tictactoe' element={<TicTacToe />} />
+            <Route path='/colourmania' element={<ColourMania />} />
+            <Route path='/hangman' element={<Hangman />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/education' element={<Education />} />
+            <Route path='/work' element={<Work />} />
+            <Route path='/contact' element={<Contact />} />
+          </Routes>
+        </BrowserRouter>
       </ThemeProvider>
     </>
 
---
+++ src/Components/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+// import { useNavigate } from 'react-router-dom';
+import Typography from '@mui/material/Typography';
+
+const Home = () => {
+    return (
+        <div>
+        <br />
+        <Typography variant="h3" sx={{textAlign: 'center', fontWeight: '900'}}>HI!</Typography>
+        {/* <Typography variant="h4" sx={{textAlign: 'center', fontWeight: '900'}}>
+            I'm Keenen Roshan, <br/>
+            a Computer Science student @UNSW majoring in Artificial Intelligence.<br /><br />
+        </Typography>
+        <Typography variant="h6" sx={{textAlign: 'center', fontWeight: '900'}}>
+             <br/>
+            Fun Fact: I coded this website from Scratch using HTML5, CSS3, Javascript (React) and the MUI Component Library!
+        </Typography> */}
+        </div>
+
+    )
+}
+
+export default Home;
